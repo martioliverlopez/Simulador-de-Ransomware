388f01b89513350f5a2d5b3a99111d798769f8cf
Marti Oliver <marti.oliver.lopez@estudiant.enti.cat>
2025-12-17 20:47:28 +0100

    Funció de xifratge d'un arxiu implementada

--- src/crypto_engine.py
+++ src/crypto_engine.py
@@ -13,10 +13,35 @@ def guardar_clau(ruta,clau):
 
     if directori and not os.path.exists(directori):
         os.makedirs(directori)
-        print(f"[INFO] Carpeta creada: {directori}")
+        print(f"[INFO] CARPETA CREADA: {directori}")
     try:
         with open(ruta, "wb") as clau_file:
             clau_file.write(clau)
             print(f"[✔] CLAU GUARDADA A {ruta}")
     except IOError as error:
         print(f"[X] ERROR AL GUARDAR: {error}")
+
+def encriptar_arxiu(ruta,clau):
+
+    if not os.path.exists(ruta):
+        print(f"[X] ERROR: ARXIU NO EXISTENT: {ruta}")
+        return None
+    
+    try:
+        xifratge = Fernet(clau)
+
+        with open(ruta, "rb") as file:
+            contingut = file.read()
+        
+        text_xifrat = xifratge.encrypt(contingut)
+
+        with open(ruta, "wb") as file:
+            file.write(text_xifrat)
+            print(f"[✔] ARXIU XIFRAT CORRECTAMENT")
+
+    except IOError as error:
+        print(f"[X] ERROR AL GUARDAR: {error}")
+    except TypeError as error:
+        print(f"[X] ERROR: DADES EN FORMAT INCORRECTE")
+    except Exception as error:
+        print(f"[X] ERROR INESPERAT: {error}")
