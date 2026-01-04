0b205c6a71763b9b19da350df81ded4c80a85700
Marc Fernández Claret <marcfc21@gmail.com>
2026-01-04 19:43:56 +0100

    Actualitzacio config.py
    tot actualitzat i incorporat fins a la SDR28 i SDR30

--- src/config.py
+++ src/config.py
@@ -1,30 +1,22 @@
+# config.py
 import os
 
-#Directori actual
-CURR_DIR = os.path.dirname(os.path.abspath(__file__))
-#Directori del simulador de ransomware
-ROOT_DIR = os.path.dirname(CURR_DIR)
+# Directori base: on es troba aquest fitxer config.py
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-#Directori Data
-DATA_DIR = os.path.join(ROOT_DIR, "data")
-#Subcarpetes del projecte
+# Tota la informacio a la subcarpeta 'data' que acabes de moure
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
 LOGS_DIR = os.path.join(DATA_DIR, "logs")
 SAND_DIR = os.path.join(DATA_DIR, "sandbox")
 SYST_DIR = os.path.join(DATA_DIR, "system") 
 ASST_DIR = os.path.join(DATA_DIR, "assets") 
 
-#--Fitxers especifics--
-#Arxiu de la clau criptografica
+# Fitxers especifics
 FILE_KEY = os.path.join(SYST_DIR, "config_sys_04.dat")
-#Arxiu dels logs
 FILE_LOGS = os.path.join(LOGS_DIR, "activity.jsonl")
-#Arxiu de la imatge per al fons de pantalla de la GUI
 FILE_LOGO = os.path.join(ASST_DIR, "logo_virus.png")
-#Nota de rescat (es crea dins de la sandbox)
-FILE_RANSOM_NOTE = os.path.join(SAND_DIR, "INSTRUCCIONS_RECUPERACIO.txt")
 
-#Llogica de creacio de carpetes si s'esborren per equivocacio
-dirs_to_create = [LOGS_DIR, SAND_DIR, SYST_DIR, ASST_DIR]
-
-for directory in dirs_to_create:
+# Creacio automatica de subcarpetes
+for directory in [LOGS_DIR, SAND_DIR, SYST_DIR, ASST_DIR]:
     os.makedirs(directory, exist_ok=True)
